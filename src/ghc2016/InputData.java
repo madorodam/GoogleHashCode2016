df1dc0ec42e466fd92c7e6253b75ca72545b0943
Cristian Saita <acatias@gmail.com>
2016-02-11 20:25:30 +0100

    Input Data

---
+++ src/ghc2016/InputData.java
@@ -0,0 +1,22 @@
+package ghc2016;
+
+public class InputData {
+	
+	public int nRows;
+	public int nCols;
+	public int nDrones;
+	public int nTurns;
+	public int maxLoad;
+	public int nProdTypes;
+	public int[] aProdWeights;
+	public int nWHs;
+	public int[] aWHRows;
+	public int[] aWHCols;
+	public int[][] mWHProdTypes;
+	public int nOrders;
+	public int[] aOrderRows;
+	public int[] aOrderCols;
+	public int[] nOrderItems; // not for persistance
+	public int[][] aOrderProdTypes;
+
+}
