3da7f8fd3af902ae2dd057becb870116330d6eb4
Cristian Saita <acatias@gmail.com>
2016-02-11 18:40:27 +0100

    c2

--- src/ghc2016/GHC2016.java
+++ src/ghc2016/GHC2016.java
@@ -3,9 +3,18 @@ package ghc2016;
 public class GHC2016 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		long startTime = System.nanoTime();		
 		
 		System.out.println("Hello, World!");
+		
+
+		
+		
+		long endTime = System.nanoTime();
+		
+		System.out.println("Running time: " + ((endTime - startTime) / 1000000.0) + " seconds");
 
 	}
 
